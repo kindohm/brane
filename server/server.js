14768317cf22b7b1f979df0c774312083034a110
Mike Hodnick <mike.hodnick@gmail.com>
2016-01-03 21:02:45 -0600

    got client-to-server comm working

--- server/server.js
+++ server/server.js
@@ -1,19 +1,40 @@
-var express = require('express');
+var express = require('express')
+	, http = require('http');
+//make sure you keep this order
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+//var express = require('express');
+//var app = express();
 
 //app.get('/', function (req, res) {
 //	res.send('Hello World!');
 //});
 
-app.use('/', express.static(__dirname + '/../public'));
+app.use('/public', express.static(__dirname + '/../public'));
 app.use('/vendor', express.static(__dirname + '/../node_modules'));
 
+//var server = app.listen(8000, function () {
+//var host = server.address().address;
+//var port = server.address().port;
+//console.log('Listening on http://%s:%s', host, port);
 
+server.listen(8000);
 
-var server = app.listen(8000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Listening on http://%s:%s', host, port);
+console.log(server);
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	socket.on('thingie', function(msg){
+		console.log('message:', msg);
+	});
+
 });
+
+
+
+//});
 
 
